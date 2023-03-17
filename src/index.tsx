fe6131d2881f27ff940691bca9b92349da09e043
Alexey <Dayzl@ya.ru>
2023-03-18 00:56:06 +0300

    Настройка webpack, добавление react, scss

---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import {render} from 'react-dom'
+
+render(
+  <div>
+    123456
+  </div>,
+  document.getElementById('root')
+)
--- webpack.config.ts
+++ webpack.config.ts
@@ -1,33 +1,25 @@
-import path from 'path'
 import webpack from 'webpack'
-import HTMLWebpackPlugin from 'html-webpack-plugin'
+import path from 'path'
+import {BuildWebpackConfig} from './config/build/buildWebpackConfig'
+import {BuildEnv, BuildPaths} from './config/build/types/config'
 
-const config: webpack.Configuration = {
-  mode: 'development',
-  entry: path.resolve(__dirname, 'src', 'index.ts'),
-  output: {
-    filename: '[name].[contenthash].js',
-    path: path.resolve(__dirname, 'bundle'),
-    clean: true,
-  },
-  plugins: [
-    new HTMLWebpackPlugin({
-      template: path.resolve(__dirname, 'public', 'index.html'),
-    }),
-    new webpack.ProgressPlugin(),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-    ],
-  },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
-  },
+export default (env: BuildEnv) => {
+  const paths: BuildPaths = {
+    entry: path.resolve(__dirname, 'src', 'index.tsx'),
+    build: path.resolve(__dirname, 'build'),
+    html: path.resolve(__dirname, 'public', 'index.html'),
+  }
+
+  const mode = env.mode || 'development'
+  const PORT = env.port || 3000
+  const isDev = mode === 'development'
+
+  const config: webpack.Configuration = BuildWebpackConfig({
+    mode,
+    paths,
+    isDev,
+    port: PORT,
+  })
+
+  return config
 }
-
-export default config
