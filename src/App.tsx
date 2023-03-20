0e43d7df922bf08602b35e6f375481f311595ce3
Alexey <dayzl@mail.ru>
2023-03-21 00:20:35 +0300

    добавление css ,разделение на темы

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
-import {Suspense} from 'react'
+import {Suspense, useState} from 'react'
 import {Route, Routes, Link} from 'react-router-dom'
-import './index.scss'
+import './styles/index.scss'
 import {AboutPageAsync} from './pages/AboutPage/AboutPage.async'
 import {MainPageAsync} from './pages/MainPage/MainPage.async'
 
+export enum Theme {
+  LIGHT = 'light',
+  DARK = 'dark'
+}
+
 const App = () => {
+  const [theme, setTheme] = useState<Theme>(Theme.LIGHT);
+  const toggleTheme = () => {
+    setTheme(theme === Theme.DARK ? Theme.LIGHT : Theme.DARK)
+  }
+
   return (
-    <div className="app">
+    <div className={`app ${theme}`}>
+      <button onClick={toggleTheme}>Toggle</button>
       <Link to="/">Главная</Link>
       <Link to="/about">О сайте</Link>
 
